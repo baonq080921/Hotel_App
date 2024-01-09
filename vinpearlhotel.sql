11c829d8ae0b2befbd5e8f0b9c0bc0702826a74c
bao <bao@gmail.com>
2024-01-09 23:45:45 +0700

    lastupdate

--- vinpearlhotel.sql
+++ vinpearlhotel.sql
@@ -1,4 +1,5 @@
 -- phpMyAdmin SQL Dump
+<<<<<<< HEAD
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
@@ -6,6 +7,15 @@
 -- Generation Time: Jan 08, 2024 at 06:46 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
+=======
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2022 at 11:19 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+>>>>>>> Last Update
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +28,11 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
+<<<<<<< HEAD
 -- Database: `vinpearlhotel`
+=======
+-- Database: `bluebirdhotel`
+>>>>>>> Last Update
 --
 
 -- --------------------------------------------------------
@@ -31,7 +45,11 @@ CREATE TABLE `emp_login` (
   `empid` int(100) NOT NULL,
   `Emp_Email` varchar(50) NOT NULL,
   `Emp_Password` varchar(50) NOT NULL
+<<<<<<< HEAD
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+=======
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+>>>>>>> Last Update
 
 --
 -- Dumping data for table `emp_login`
@@ -61,16 +79,24 @@ CREATE TABLE `payment` (
   `meal` varchar(30) NOT NULL,
   `mealtotal` double(8,2) NOT NULL,
   `finaltotal` double(8,2) NOT NULL
+<<<<<<< HEAD
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+=======
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+>>>>>>> Last Update
 
 --
 -- Dumping data for table `payment`
 --
 
 INSERT INTO `payment` (`id`, `Name`, `Email`, `RoomType`, `Bed`, `NoofRoom`, `cin`, `cout`, `noofdays`, `roomtotal`, `bedtotal`, `meal`, `mealtotal`, `finaltotal`) VALUES
+<<<<<<< HEAD
 (51, 'Minh Ngo', 'minh@gmail.com', 'Guest House', 'Quad', 1, '2023-12-28', '2024-01-05', 8, 12000.00, 480.00, 'Breakfast', 960.00, 13440.00),
 (58, 'Ngo Quang A', 'A@gmail.com', 'Superior Room', 'Single', 1, '2024-01-05', '2024-01-07', 2, 6000.00, 60.00, 'Full Board', 240.00, 6300.00),
 (59, 'Ngo Quang B', 'B@gmail.com', 'Deluxe Room', 'Single', 2, '2024-01-05', '2024-01-08', 3, 12000.00, 120.00, 'Breakfast', 120.00, 12240.00);
+=======
+(41, 'Bao Ngo', 'bao992001pth@gmail.com', 'Single Room', 'Single', 1, '2023-12-09', '2024-01-105', 1, 1000.00, 10.00, 'Room only', 0.00, 1010.00);
+>>>>>>> Last Update
 
 -- --------------------------------------------------------
 
@@ -82,13 +108,22 @@ CREATE TABLE `room` (
   `id` int(30) NOT NULL,
   `type` varchar(50) NOT NULL,
   `bedding` varchar(50) NOT NULL
+<<<<<<< HEAD
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+=======
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+>>>>>>> Last Update
 
 --
 -- Dumping data for table `room`
 --
 
 INSERT INTO `room` (`id`, `type`, `bedding`) VALUES
+<<<<<<< HEAD
+=======
+(4, 'Superior Room', 'Single'),
+(6, 'Superior Room', 'Triple'),
+>>>>>>> Last Update
 (7, 'Superior Room', 'Quad'),
 (8, 'Deluxe Room', 'Single'),
 (9, 'Deluxe Room', 'Double'),
@@ -103,6 +138,7 @@ INSERT INTO `room` (`id`, `type`, `bedding`) VALUES
 (23, 'Deluxe Room', 'Single'),
 (24, 'Deluxe Room', 'Triple'),
 (27, 'Guest House', 'Double'),
+<<<<<<< HEAD
 (30, 'Deluxe Room', 'Single'),
 (31, '', ''),
 (32, '', ''),
@@ -110,6 +146,9 @@ INSERT INTO `room` (`id`, `type`, `bedding`) VALUES
 (34, 'Superior Room', 'Single'),
 (35, 'Deluxe Room', 'Single'),
 (36, 'Deluxe Room', 'Single');
+=======
+(30, 'Deluxe Room', 'Single');
+>>>>>>> Last Update
 
 -- --------------------------------------------------------
 
@@ -131,16 +170,24 @@ CREATE TABLE `roombook` (
   `cout` date NOT NULL,
   `nodays` int(50) NOT NULL,
   `stat` varchar(30) NOT NULL
+<<<<<<< HEAD
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+=======
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+>>>>>>> Last Update
 
 --
 -- Dumping data for table `roombook`
 --
 
 INSERT INTO `roombook` (`id`, `Name`, `Email`, `Country`, `Phone`, `RoomType`, `Bed`, `Meal`, `NoofRoom`, `cin`, `cout`, `nodays`, `stat`) VALUES
+<<<<<<< HEAD
 (53, 'Nguyen Van A', 'van@gmail.com', 'Vietnam', '01223213214', 'Single Room', 'Single', 'Full Board', '1', '2024-01-03', '2024-01-05', 2, 'Confirm'),
 (58, 'Ngo Quang A', 'A@gmail.com', 'Vietnam', '0385633120', 'Superior Room', 'Single', 'Full Board', '1', '2024-01-05', '2024-01-07', 2, 'Confirm'),
 (59, 'Ngo Quang B', 'B@gmail.com', 'Sweden', '0385633121', 'Deluxe Room', 'Single', 'Breakfast', '2', '2024-01-05', '2024-01-08', 3, 'Confirm');
+=======
+(41, 'Bao Ngo', 'bao992001pth@gmail.co,', 'VietNam', '0385633120', 'Single Room', 'Single', 'Room only', '1', '2023-12-09', '2024-01-05', 1, 'Confirm');
+>>>>>>> Last Update
 
 -- --------------------------------------------------------
 
@@ -152,12 +199,18 @@ CREATE TABLE `signup` (
   `UserID` int(100) NOT NULL,
   `Username` varchar(50) NOT NULL,
   `Email` varchar(50) NOT NULL,
+<<<<<<< HEAD
   `Password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+=======
+  `Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+>>>>>>> Last Update
 
 --
 -- Dumping data for table `signup`
 --
+<<<<<<< HEAD
 
 INSERT INTO `signup` (`UserID`, `Username`, `Email`, `Password`) VALUES
 (12, 'bao', 'bao992001pth@gmail.com', '$2y$10$2YKHhJkIENCvMENMfY5hhOsTNH36WiRic.cU5f74CdwGdeQcELQgm'),
@@ -166,6 +219,10 @@ INSERT INTO `signup` (`UserID`, `Username`, `Email`, `Password`) VALUES
 (15, 'bao89', 'bao123@gmail.com', '$2y$10$qlK1.g3IZxBk5rWTTdhjy.tRDVvpyrYN31Zj2/2vteB6UUY1UpYtO'),
 (16, 'QuangA', 'A@gmail.com', '$2y$10$mgc1GE8UhmLccZo8mWhLiub.1ps0lOqQsUOJGE5BnY0aDzrbxUp1S'),
 (17, 'C', 'c@gmail.com', '$2y$10$TAuUtHM8.Tl2.rZDgZXvPeY7tTwOZharqpVGMV25lbf2oDDMk9nDW');
+=======
+INSERT INTO `signup` (`UserID`, `Username`, `Email`, `Password`) VALUES
+(1, 'bao', 'bao992001pth@gmail.com', '123');
+>>>>>>> Last Update
 
 -- --------------------------------------------------------
 
@@ -177,24 +234,39 @@ CREATE TABLE `staff` (
   `id` int(30) NOT NULL,
   `name` varchar(30) NOT NULL,
   `work` varchar(30) NOT NULL
+<<<<<<< HEAD
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+=======
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+>>>>>>> Last Update
 
 --
 -- Dumping data for table `staff`
 --
 
 INSERT INTO `staff` (`id`, `name`, `work`) VALUES
+<<<<<<< HEAD
 (1, 'Bao Ngo', 'Manager'),
+=======
+(1, 'Tushar pankhaniya', 'Manager'),
+(3, 'rohit patel', 'Cook'),
+>>>>>>> Last Update
 (4, 'Dipak', 'Cook'),
 (5, 'tirth', 'Helper'),
 (6, 'mohan', 'Helper'),
 (7, 'shyam', 'cleaner'),
 (8, 'rohan', 'weighter'),
+<<<<<<< HEAD
 (10, 'nikunj', 'weighter'),
 (12, 'Minh', 'cleaner'),
 (13, 'Luis', 'Cook'),
 (14, 'Ramsey', 'Chef'),
 (15, 'QuangB', 'Helper');
+=======
+(9, 'hiren', 'weighter'),
+(10, 'nikunj', 'weighter'),
+(11, 'rekha', 'Cook');
+>>>>>>> Last Update
 
 --
 -- Indexes for dumped tables
@@ -250,27 +322,47 @@ ALTER TABLE `emp_login`
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
+<<<<<<< HEAD
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+=======
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+>>>>>>> Last Update
 
 --
 -- AUTO_INCREMENT for table `roombook`
 --
 ALTER TABLE `roombook`
+<<<<<<< HEAD
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+=======
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+>>>>>>> Last Update
 
 --
 -- AUTO_INCREMENT for table `signup`
 --
 ALTER TABLE `signup`
+<<<<<<< HEAD
   MODIFY `UserID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+=======
+  MODIFY `UserID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+>>>>>>> Last Update
 
 --
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
+<<<<<<< HEAD
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+=======
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+>>>>>>> Last Update
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+<<<<<<< HEAD
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+=======
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+>>>>>>> Last Update
